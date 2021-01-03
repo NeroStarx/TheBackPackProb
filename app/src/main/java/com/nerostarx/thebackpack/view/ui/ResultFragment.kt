15107c2fb6e625cdeac7fb5bc8590e365f6376d3
Aaron NeroStarx <35573892+NeroStarx@users.noreply.github.com>
2021-01-04 00:08:07 +0100

    added the design of the fragment_result.xml

--- app/src/main/java/com/nerostarx/thebackpack/view/ui/ResultFragment.kt
+++ app/src/main/java/com/nerostarx/thebackpack/view/ui/ResultFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.nerostarx.thebackpack.R
 
 class ResultFragment : Fragment() {
@@ -13,7 +17,12 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val itemsRecycler: RecyclerView = view.findViewById(R.id.final_items_recycler)
+
     }
 }
