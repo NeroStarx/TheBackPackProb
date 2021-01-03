8192b67dca2d54bc24ffa55a2a8afb2da9e69307
Aaron NeroStarx <35573892+NeroStarx@users.noreply.github.com>
2021-01-03 23:19:41 +0100

    added the design of the fragment_selection.xml

--- app/src/main/java/com/nerostarx/thebackpack/view/ui/SelectionFragment.kt
+++ app/src/main/java/com/nerostarx/thebackpack/view/ui/SelectionFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.nerostarx.thebackpack.R
 
 
@@ -14,7 +19,18 @@ class SelectionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_selection, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val confirmButton: Button = view.findViewById(R.id.confirm_items_button)
+        val title: TextView = view.findViewById(R.id.selection_title)
+        val itemsRecycler: RecyclerView = view.findViewById(R.id.items_recycler)
+
+        confirmButton.setOnClickListener{
+            val navController = findNavController()
+            navController.navigate(R.id.to_result_action)
+        }
     }
 }
